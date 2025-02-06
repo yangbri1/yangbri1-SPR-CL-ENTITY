78d623f684444d573559a7155edda3016fc8d492
Brian Yang <yangbri1@msu.edu>
2025-02-06 21:27:06 +0000

    Add '@Entity' annotation to whole overhead 'Product.java' class to indicate this class is mapped to a relation in DB. Include '@Id' annotation onto class fields to establish PRIMARY_KEY of DB table.

--- src/main/java/Lab/Model/Product.java
+++ src/main/java/Lab/Model/Product.java
@@ -22,8 +22,10 @@ import java.util.Objects;
  */
 
 // @TODO - Add an annotation to indicate that this class is mapped to a relation in the database:
+@Entity
 public class Product {
     // TODO - Add an annotation to indicate that this field is the primary key for the corresponding relation:
+    @Id
     private long productID;
     private String name;
     private String description;
